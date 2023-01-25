324325bff2f117ec8499802c1ec10a641905ac74
charlcl180 <93603028+charlcl180@users.noreply.github.com>
2023-01-25 15:24:03 -0500

    debut étape 3

--- s20223-log210-01-lab00-charlcl180-main/test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ s20223-log210-01-lab00-charlcl180-main/test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,6 +1,17 @@
 // Vous devez insérer les nouveaux tests ici
-import { assert } from 'console';
 import 'jest-extended';
+import supertest from 'supertest'
+import app from '../../../src/app'
+
+const request = supertest(app)
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Jean-Marc';
+
+
+beforeAll(async () => {
+  await request.post('/api/v1/jeu/redemarrerJeu').send({nom: testNom1})
+  await request.post('/api/v1/jeu/redemarrerJeu').send({nom: testNom1})
+});
 
 describe('redemarrerJeu.test.ts', () => {
   it("should implement test", async () => {
